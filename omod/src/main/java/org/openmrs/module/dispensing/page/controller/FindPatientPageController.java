5b0f739ef07bbd3efc5e689c7563f4e6957d6527
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-03-24 16:21:31 -0700

    UHM-1210 - Limit last medication dispensed shown

--- omod/src/main/java/org/openmrs/module/dispensing/page/controller/FindPatientPageController.java
+++ omod/src/main/java/org/openmrs/module/dispensing/page/controller/FindPatientPageController.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.dispensing.page.controller;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.openmrs.Location;
 import org.openmrs.module.appui.UiSessionContext;
 import org.openmrs.module.dispensing.DispensedMedication;
@@ -8,41 +10,26 @@ import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nlima
- * Date: 10/25/13
- * Time: 1:59 PM
- * To change this template use File | Settings | File Templates.
- */
 public class FindPatientPageController {
 
     public Location currentLocation;
 
     public void controller(UiSessionContext emrContext,
-                           PageModel model, @SpringBean("dispensingService") DispensingService dispensingService){
-
+                           PageModel model,
+                           @SpringBean("dispensingService") DispensingService dispensingService){
 
         currentLocation = emrContext.getSessionLocation();
         List<Location> locationList = new ArrayList<Location>();
         locationList.add(currentLocation);
 
-        Calendar cal = Calendar.getInstance();
-        Date currentTime = cal.getTime();
-        cal.add(Calendar.MONTH, -6);
-        Date sixMonthsAgo = cal.getTime();
+        Date showDataSince = new DateTime().minus(Days.THREE).toDate();
 
-        List<DispensedMedication> dispensedMedicationList = dispensingService.getDispensedMedication(null,locationList,sixMonthsAgo, currentTime,null,null);
+        List<DispensedMedication> dispensedMedicationList = dispensingService.getDispensedMedication(null, locationList, showDataSince, null ,null, null);
 
         model.addAttribute("dispensedMedicationListbyLocation", dispensedMedicationList);
-
-
-
     }
-
 
 }
